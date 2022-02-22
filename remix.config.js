b608270c16871756b1012a59c7863bb6fd5abb44
berrylover03 <80674744+berrylover03@users.noreply.github.com>
2022-02-22 06:05:00 -0500

    Initialize deploy/arc

---
+++ remix.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  appDirectory: 'app',
+  browserBuildDirectory: './public/build',
+  publicPath: '/_static/build/',
+  serverBuildDirectory: './backend/build',
+  devServerPort: 3334,
+};
